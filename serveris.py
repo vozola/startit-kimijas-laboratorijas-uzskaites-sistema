197446b0664c7b2d8af2a4b56c70b9034dfffaec
Valda Ozola <vozola11@gmail.com>
2020-12-29 09:21:13 +0000

    Izveidots serveris

--- serveris.py
+++ serveris.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Labrīt!"
-
+    return "Labrīt!!!"
+@app.route('/sveiki')
+def sveiki():
+    return"Nav vairs nekāds rīts!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
