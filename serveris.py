ed0e5e40bd3e0afb9b85a479297e7c35ed982cd4
Valda Ozola <vozola11@gmail.com>
2020-12-29 09:46:56 +0000

    pievienots rout

--- serveris.py
+++ serveris.py
@@ -10,6 +10,9 @@ def index():
 @app.route('/sveiki')
 def sveiki():
     return"Nav vairs nekāds rīts!"
+@app.route('/sveiki/<vards>')
+def sveikiPersona():
+    return"Sveiki{}".format(vards) 
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
